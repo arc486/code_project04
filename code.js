ebc4c979851656e386eec3c847e4d5f24dddfc0c
arc486 <77684570+arc486@users.noreply.github.com>
2021-02-19 14:25:10 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,13 @@
+//  javascript Document 
+
+console.log('this class is really making me happry becuase i get to learn soemhthing new besides html and hope to learn more')
+
+var firstname= adam ;
+
+alert ('my name is adam cintron');
+
+let lastname = cintron;
+const age = 20 ; 
+
+
+alert('i am 20 years old ')
